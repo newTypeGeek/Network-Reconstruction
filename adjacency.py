2e27ad77f0422d8c8564b0ca74f062d3b89a165a
bpht <bennyphtam@gmail.com>
2019-10-27 21:50:51 +0800

    Added warning for ER random when p = 0, p = 1

--- adjacency.py
+++ adjacency.py
@@ -21,24 +21,35 @@ def er_random(N, p):
     assert type(N) == int and N > 0, "Number of node must be a positive integer"
     assert (type(p) == int or type(p) == float), "Connection probability must be a real number"
     assert p >= 0 and p <= 1, "Connection probability must be within 0 to 1 (inclusive)"
+   
+    if p == 0:
+        print("[WARN] p = 0, a network has no links")
+        A = np.zeros((N,N))
+        return A
+
+    elif p == 1:
+        print("[WARN] p = 1, a network is fully connected")
+        A = np.ones((N,N))
+        return A
+
+    else:
+        # Generate a random matrix with size N x N
+        # Each element takes value between 0 and 1
+        A = np.random.rand(N, N)
     
-    # Generate a random matrix with size N x N
-    # Each element takes value between 0 and 1
-    A = np.random.rand(N, N)
     
+        # Set the connectivity based on input probability threshold p
+        A = A < p
+        A = A.astype(int)
     
-    # Set the connectivity based on input probability threshold p
-    A = A < p
-    A = A.astype(int)
+        # Extract only the upper triangle elements
+        # And set other (lower triangle + diagonal) to zero
+        A = np.triu(A, 1)
     
-    # Extract only the upper triangle elements
-    # And set other (lower triangle + diagonal) to zero
-    A = np.triu(A, 1)
-    
-    # Symmetrize the matrix
-    A = A + A.T
+        # Symmetrize the matrix
+        A = A + A.T
      
-    return A
+        return A
 
 
 ############################################################
